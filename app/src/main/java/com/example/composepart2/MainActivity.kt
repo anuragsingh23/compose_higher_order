de964a159bcea63279ecd2fe3eefbc10e0bf1ad1
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-05-17 15:24:04 +0530

    Column and row compose function and extension function

--- app/src/main/java/com/example/composepart2/MainActivity.kt
+++ app/src/main/java/com/example/composepart2/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.composepart2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composepart2.ui.theme.ComposePart2Theme
 import com.example.composepart2.ui.theme.Typography
 
@@ -18,41 +20,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePart2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Column {
-                        Meet(name = "F1-Furious")
-                        Greeting("Android")
-                    }
 
+            ComposePart2Theme {
+                androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
+                    Column (modifier = Modifier.fillMaxSize()
+                        , horizontalAlignment = Alignment.CenterHorizontally)
+                    {
+                        CustomItem(color = MaterialTheme.colors.secondaryVariant, weight =3f )
+                        CustomItem(weight = 1f)
+                    }
+                }
+            }
                 }
             }
         }
+
+
+
+
+@Composable
+//Column extension function
+fun ColumnScope.CustomItem(color: Color = MaterialTheme.colors.onPrimary , weight : Float ) {
+    Surface(
+        modifier = Modifier
+            .weight(weight)
+            .width(200.dp)
+            ,color = color,
+    ) {
+
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Composable
-fun Meet(name: String){
-    Text(text = name,
-        style = Typography.h6
-        )
-
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePart2Theme {
-        Column {
-            Meet(name = "F1-Furious")
-            Greeting("Android")
+        Column (modifier = Modifier.fillMaxSize()
+        , horizontalAlignment = Alignment.CenterHorizontally)
+        {
+             CustomItem(color = MaterialTheme.colors.onError, weight =3f )
+            CustomItem(weight = 1f)
         }
     }
 }
