80884193a263ab7b9f45eb89e0f81a3e4bd824b3
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-05-17 15:43:27 +0530

    How to Use Box Compose Function

--- app/src/main/java/com/example/composepart2/MainActivity.kt
+++ app/src/main/java/com/example/composepart2/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.composepart2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,8 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepart2.ui.theme.ComposePart2Theme
 import com.example.composepart2.ui.theme.Typography
+import java.security.AllPermission
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +27,8 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             ComposePart2Theme {
-                androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
-                    Column (modifier = Modifier.fillMaxSize()
-                        , horizontalAlignment = Alignment.CenterHorizontally)
-                    {
-                        CustomItem(color = MaterialTheme.colors.secondaryVariant, weight =3f )
-                        CustomItem(weight = 1f)
-                    }
+                    Surface(color = MaterialTheme.colors.background) {
+                   Greeting()
                 }
             }
                 }
@@ -39,14 +39,19 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-//Column extension function
-fun ColumnScope.CustomItem(color: Color = MaterialTheme.colors.onPrimary , weight : Float ) {
-    Surface(
-        modifier = Modifier
-            .weight(weight)
-            .width(200.dp)
-            ,color = color,
-    ) {
+//Box compose function
+fun Greeting(){
+    Box(modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.TopCenter){
+        Box(modifier = Modifier
+            .height(400.dp)
+            .width(400.dp)
+            .verticalScroll(rememberScrollState())
+            .background(color = Color.Green)) {
+
+            Text(text = "i am anurag shekhawat", color = MaterialTheme.colors.onPrimary, fontSize = 60.sp)
+
+        }
 
     }
 }
@@ -57,11 +62,6 @@ fun ColumnScope.CustomItem(color: Color = MaterialTheme.colors.onPrimary , weigh
 @Composable
 fun DefaultPreview() {
     ComposePart2Theme {
-        Column (modifier = Modifier.fillMaxSize()
-        , horizontalAlignment = Alignment.CenterHorizontally)
-        {
-             CustomItem(color = MaterialTheme.colors.onError, weight =3f )
-            CustomItem(weight = 1f)
-        }
+        Greeting()
     }
 }
