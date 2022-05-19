f7231072777e0dc02ea56ed954d6d4c8637dc157
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-05-20 01:50:37 +0530

    How to use Text Feilds 1.editable text feilds 2.passwords type 3. single line 4.icons 5.trailing icons

--- app/src/main/java/com/example/composepart2/MainActivity.kt
+++ app/src/main/java/com/example/composepart2/MainActivity.kt
@@ -3,26 +3,27 @@ package com.example.composepart2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composepart2.ui.CardExpandable
 import com.example.composepart2.ui.theme.ComposePart2Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,43 +32,90 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             ComposePart2Theme {
-                    Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.background) {
 
-                        CardExpandable()
+                    Greeting()
                 }
             }
-                }
-            }
+
         }
+    }
+}
 
 
 
 
 @Composable
-//Box compose function
-fun Greeting(
-    normal :String,
-    script : String
-){
-    Text(text = buildAnnotatedString {
-        withStyle (
-            style = SpanStyle(
-                fontSize = MaterialTheme.typography.h3.fontSize
+fun Greeting(){
+    Column(modifier = Modifier.fillMaxSize().padding(20.dp),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Top,
+    ) {
+        var text by remember { mutableStateOf("type here") }
+
+        TextField(value = "please text here ", onValueChange = {
+                text = it
+
+        },
+            readOnly = false,
+            label = {
+                Text(text = "Title")
+            },
+            singleLine = true,
+            maxLines = 2,
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Email,contentDescription = null)
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Menu,contentDescription = null)
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+
+                }
             )
-                ){
-            append(normal)
-        }
-    })
-    Text(text = buildAnnotatedString {
-        withStyle (
-            style = SpanStyle(
-                fontSize = MaterialTheme.typography.h6.fontSize,
-                baselineShift = BaselineShift.Superscript
+        )
+        OutlinedTextField(value = "please text here ", onValueChange = {
+            text = it
+
+        },
+            readOnly = false,
+            label = {
+                Text(text = "Title")
+            },
+            singleLine = true,
+            maxLines = 2,
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Email,contentDescription = null)
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Menu,contentDescription = null)
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+
+                }
             )
-        ){
-            append(normal)
-        }
-    })
+        )
+
+    }
+
 }
 
 
@@ -76,6 +124,6 @@ fun Greeting(
 @Composable
 fun DefaultPreview() {
     ComposePart2Theme {
-        Greeting("inder","63")
+        Greeting()
     }
 }
